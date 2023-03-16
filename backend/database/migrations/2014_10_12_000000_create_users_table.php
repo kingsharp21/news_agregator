99a1a9eb278e8fdbcfe849126b07044d9a1718f4
kingsharp21 <kingsharpn3@gmail.com>
2023-03-16 12:39:57 +0000

    dockerize app

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Preference extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class Preference extends Migration
      */
     public function up()
     {
-        //
-        Schema::create('preference', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->string('authors');
-            $table->string('sources');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class Preference extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('users');
     }
 }
